c6e3d9cea54bc0fd8b6a34490e39e688531fbba8
Alvaro Leiva Geisse <aleivag@gmail.com>
2024-08-06 14:49:31 -0700

    typing

--- src/cstq/__init__.py
+++ src/cstq/__init__.py
@@ -4,6 +4,6 @@
 
 from cstq.matchers_helpers import obj2m
 from cstq.obj2cst import obj2cst
-from cstq.query import Query
+from cstq.query import Query, CollectionOfNodes
 
-__all__ = ["Query", "obj2cst"]
+__all__ = ["Query", "obj2cst", "obj2m", "CollectionOfNodes"]
