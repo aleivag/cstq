7933da94200ff9b853d8b977f17d485baf860f63
Alvaro Leiva Geisse <aleivag@gmail.com>
2024-09-26 07:42:56 -0700

    test gh action

--- src/cstq/__version__.py
+++ src/cstq/__version__.py
@@ -1,4 +1,8 @@
 # SPDX-FileCopyrightText: 2023-present Alvaro Leiva <aleivag@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+# Warning:
+#   modifying this file will trigger to publish anew version
+
 __version__ = "0.0.19"
